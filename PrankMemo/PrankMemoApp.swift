043040cd751e2a0748754824b71104c0e4e064de
amefure <shachi10969@gmail.com>
2024-08-19 19:28:01 +0900

    Initial Commit

---
+++ PrankMemo/PrankMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrankMemoApp.swift
+//  PrankMemo
+//
+//  Created by t&a on 2024/08/19.
+//
+
+import SwiftUI
+
+@main
+struct PrankMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
